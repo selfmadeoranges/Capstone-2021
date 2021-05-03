ac1846d97fc75eb228029055be6b099d242fa42b
tkdwns6292 <tkdwns6292@naver.com>
2021-05-03 16:12:54 +0900

    add img

--- translate.py
+++ translate.py
@@ -1,31 +1,39 @@
 import cv2
-import numpy as np
+import numpy as np 
+ 
+i = 0
+while True:
+   img = cv2.imread('./data_img/yoonji/{}.jpg'.format(i))
+   rows,cols = img.shape[0:2]  # 영상의 크기 
+ 
+   m0 = cv2.getRotationMatrix2D((cols/2,rows/2),0,2) 
+   m1 = cv2.getRotationMatrix2D((cols/2,rows/2),25,1.5)
+   m2 = cv2.getRotationMatrix2D((cols/2,rows/2),360-25,1.5)
+   m3 = cv2.getRotationMatrix2D((cols/2,rows/2),0,0.5)    
+ 
+   #---② 변환 행렬 적용
+   img0 = cv2.warpAffine(img, m0,(cols, rows))
+   img1 = cv2.warpAffine(img, m1,(cols, rows))
+   img2 = cv2.warpAffine(img, m2,(cols, rows)) 
+   img3 = cv2.warpAffine(img, m3,(cols, rows)) 
+ 
+   #cv2.imshow('origin',img)
+   #cv2.imshow('zoom',img0)
+   #cv2.imshow("25", img25)
+   #cv2.imshow("45", img45)
+   #cv2.waitKey(0)
+   #cv2.destroyAllWindows() 
+ 
+   # outfilename = 'C:/Temp/test/data_img/SangJun00000-{}.jpg'.format(i)
+   outfilename = './processed__img/yoonji/{}-1.jpg'.format(i)
+   cv2.imwrite(outfilename, img0)
+   outfilename = './processed__img/yoonji/{}-2.jpg'.format(i)
+   cv2.imwrite(outfilename, img1)
+   outfilename = './processed__img/yoonji/{}-3.jpg'.format(i)
+   cv2.imwrite(outfilename, img2)
+   outfilename = './processed__img/yoonji/{}-4.jpg'.format(i)
+   cv2.imwrite(outfilename, img3)
+   i=i+1
 
-img = cv2.imread('./data_img/SangJun00000.jpg')
-rows,cols = img.shape[0:2]  # 영상의 크기
-
-m0 = cv2.getRotationMatrix2D((cols/2,rows/2),0,1.5)
-
-m25 = cv2.getRotationMatrix2D((cols/2,rows/2),25,1.5) 
-# 회전축:중앙, 각도:90, 배율:1.5
-m45 = cv2.getRotationMatrix2D((cols/2,rows/2),45,1.5) 
-
-#---② 변환 행렬 적용
-img0 = cv2.warpAffine(img, m0,(cols, rows))
-img25 = cv2.warpAffine(img, m25,(cols, rows))
-img45 = cv2.warpAffine(img, m45,(cols, rows))
-
-cv2.imshow('origin',img)
-cv2.imshow('zoom',img0)
-cv2.imshow("25", img25)
-cv2.imshow("45", img45)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# outfilename = 'C:/Temp/test/data_img/SangJun00000-{}.jpg'.format(i)
-outfilename = 'C:/Temp/test/SangJun00000-1.jpg'
-cv2.imwrite(outfilename, img0)
-outfilename = 'C:/Temp/test/SangJun00000-2.jpg'
-cv2.imwrite(outfilename, img25)
-outfilename = 'C:/Temp/test/SangJun00000-3.jpg'
-cv2.imwrite(outfilename, img45)
+   if i == 300 :
+       break
