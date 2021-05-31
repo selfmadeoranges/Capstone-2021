3d3008addba3dea7bc0298fc7aaa2f8efb41c049
tkdwns6292 <tkdwns6292@naver.com>
2021-06-01 02:09:50 +0900

    add img

--- cam_Capture.py
+++ cam_Capture.py
@@ -2,17 +2,18 @@ import cv2
 
 scaler = 0.2
 # video_file = "../img/big_buck.avi" # 동영상 파일 경로
-i = 0
-cap = cv2.VideoCapture(0) # webcam 캡쳐 객체 생성  ---①
+i = 6
+cap = cv2.VideoCapture('222.mp4') # webcam 캡쳐 객체 생성  ---①
 
 if cap.isOpened():                 # 캡쳐 객체 초기화 확인
     while True:
         ret, img = cap.read()      # 다음 프레임 읽기      --- ②
         # img = cv2.resize(img, (int(img.shape[1] * scaler), int(img.shape[0] * scaler))) #사이즈 축소
+        img = cv2.flip(img, 1) #w좌우 반전
         if ret:                     # 프레임 읽기 정상
             cv2.imshow('camara', img) # 화면에 표시  --- ③
             if cv2.waitKey(1) != -1:            # 아무 키나 누르면
-                outfilename = 'C:/Temp/test/data_img/{}.jpg'.format(i)
+                outfilename = './data_img/1/sangjun0000{}.jpg'.format(i)
                 cv2.imwrite(outfilename, img) # 프레임을 'photo.jpg'에 저장
                 i+=1
         else:                       # 다음 프레임 읽을 수 없슴,
--- translate.py
+++ translate.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np 
  
-i = 0
+i = 6
 while True:
-   img = cv2.imread('./data_img/yoonji/{}.jpg'.format(i))
+   img = cv2.imread('./data_img/1/sangjun0000{}.jpg'.format(i))
    rows,cols = img.shape[0:2]  # 영상의 크기 
  
    m0 = cv2.getRotationMatrix2D((cols/2,rows/2),0,2) 
@@ -25,13 +25,13 @@ while True:
    #cv2.destroyAllWindows() 
  
    # outfilename = 'C:/Temp/test/data_img/SangJun00000-{}.jpg'.format(i)
-   outfilename = './processed__img/yoonji/{}-1.jpg'.format(i)
+   outfilename = './processed__img/1/sangjun0000{}-1.jpg'.format(i)
    cv2.imwrite(outfilename, img0)
-   outfilename = './processed__img/yoonji/{}-2.jpg'.format(i)
+   outfilename = './processed__img/1/sangjun0000{}-2.jpg'.format(i)
    cv2.imwrite(outfilename, img1)
-   outfilename = './processed__img/yoonji/{}-3.jpg'.format(i)
+   outfilename = './processed__img/1/sangjun0000{}-3.jpg'.format(i)
    cv2.imwrite(outfilename, img2)
-   outfilename = './processed__img/yoonji/{}-4.jpg'.format(i)
+   outfilename = './processed__img/1/sangjun0000{}-4.jpg'.format(i)
    cv2.imwrite(outfilename, img3)
    i=i+1
 
