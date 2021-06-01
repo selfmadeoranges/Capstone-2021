a56d3ffecf3f111539a68d7f33e1f66b3d3f59e3
sangjunSeung <tkdwns6292@naver.com>
2021-06-01 10:59:12 +0900

    add img & final test

--- translate.py
+++ translate.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np 
  
-i = 6
+i = 0
 while True:
-   img = cv2.imread('./data_img/1/sangjun0000{}.jpg'.format(i))
+   img = cv2.imread('./data_img/1/jongwon0000{}.jpg'.format(i))
    rows,cols = img.shape[0:2]  # 영상의 크기 
  
-   m0 = cv2.getRotationMatrix2D((cols/2,rows/2),0,2) 
+   m0 = cv2.getRotationMatrix2D((cols/2,rows/2),0,1.5) 
    m1 = cv2.getRotationMatrix2D((cols/2,rows/2),25,1.5)
    m2 = cv2.getRotationMatrix2D((cols/2,rows/2),360-25,1.5)
    m3 = cv2.getRotationMatrix2D((cols/2,rows/2),0,0.5)    
@@ -25,13 +25,13 @@ while True:
    #cv2.destroyAllWindows() 
  
    # outfilename = 'C:/Temp/test/data_img/SangJun00000-{}.jpg'.format(i)
-   outfilename = './processed__img/1/sangjun0000{}-1.jpg'.format(i)
+   outfilename = './processed__img/1/jongwon0000{}-1.jpg'.format(i)
    cv2.imwrite(outfilename, img0)
-   outfilename = './processed__img/1/sangjun0000{}-2.jpg'.format(i)
+   outfilename = './processed__img/1/jongwon0000{}-2.jpg'.format(i)
    cv2.imwrite(outfilename, img1)
-   outfilename = './processed__img/1/sangjun0000{}-3.jpg'.format(i)
+   outfilename = './processed__img/1/jongwon0000{}-3.jpg'.format(i)
    cv2.imwrite(outfilename, img2)
-   outfilename = './processed__img/1/sangjun0000{}-4.jpg'.format(i)
+   outfilename = './processed__img/1/jongwon0000{}-4.jpg'.format(i)
    cv2.imwrite(outfilename, img3)
    i=i+1
 
