cebe5c00eff86f787f6c5b4a8372f09dea50472c
sangjunSeung <tkdwns6292@naver.com>
2021-06-01 11:18:01 +0900

    code change

--- translate.py
+++ translate.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np 
  
 i = 0
+username = 'sangjun'
 while True:
-   img = cv2.imread('./data_img/1/sangjun0000{}.jpg'.format(i))
+   img = cv2.imread('./data_img/1/{}0000{}.jpg'.format(username,i))
    rows,cols = img.shape[0:2]  # 영상의 크기 
  
    m0 = cv2.getRotationMatrix2D((cols/2,rows/2),0,1.5) 
@@ -17,13 +18,13 @@ while True:
    img2 = cv2.warpAffine(img, m2,(cols, rows)) 
    img3 = cv2.warpAffine(img, m3,(cols, rows)) 
  
-   outfilename = './processed__img/1/sangjun0000{}-1.jpg'.format(i)
+   outfilename = './processed__img/1/{}0000{}-1.jpg'.format(username,i)
    cv2.imwrite(outfilename, img0)
-   outfilename = './processed__img/1/sangjun0000{}-2.jpg'.format(i)
+   outfilename = './processed__img/1/{}0000{}-2.jpg'.format(username,i)
    cv2.imwrite(outfilename, img1)
-   outfilename = './processed__img/1/sangjun0000{}-3.jpg'.format(i)
+   outfilename = './processed__img/1/{}0000{}-3.jpg'.format(username,i)
    cv2.imwrite(outfilename, img2)
-   outfilename = './processed__img/1/sangjun0000{}-4.jpg'.format(i)
+   outfilename = './processed__img/1/{}0000{}-4.jpg'.format(username,i)
    cv2.imwrite(outfilename, img3)
    i=i+1
 
